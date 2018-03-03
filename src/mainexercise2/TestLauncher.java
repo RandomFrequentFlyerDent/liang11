f7c333f3b332cd6e9c437d4f1e236aa72422f099
RandomFrequentFlyerDent <n.everts@gmail.com>
2018-03-03 17:01:04 +0100

    commit tests

--- src/mainexercise2/TestLauncher.java
+++ src/mainexercise2/TestLauncher.java
@@ -6,14 +6,17 @@ public class TestLauncher {
 		Person person1 = new Person();
 		Person person2 = new Student();
 		Person person3 = new Employee();
+		Person person4 = new Person();
 
 		person1.setName("Wiebe");
 		person2.setName("Nathalie");
 		person3.setName("Not-Us");
+		person4.setName("Something Else");
 
 		System.out.println(person1.toString());
 		System.out.println(person2.toString());
 		System.out.println(person3.toString());
+		System.out.println(person4.toString());
 
 	}
 
